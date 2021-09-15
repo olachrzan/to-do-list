f16354193fb54a1764ed3740c0a585a5efc8e862
Ola Chrzan <ola.chrzan16@gmail.com>
2021-09-15 14:28:40 +0200

    Adding new tasks

--- JS/script.js
+++ JS/script.js
@@ -25,9 +25,29 @@
       document.querySelector(".js-taskList").innerHTML = htmlString;
    };
 
+   const addNewTask = (newTaskContent) => {
+      tasks.push({
+         content: newTaskContent,
+      });
+      render();
+   };
+
+   const onFormSubmit = (event) => {
+      event.preventDefault();
+
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+      if (newTaskContent === "") {
+         return;
+      }
+      addNewTask(newTaskContent);
+   };
 
    const init = () => {
       render();
+
+      const form = document.querySelector(".js-form");
+      form.addEventListener("submit", onFormSubmit);
    };
 
    init();
